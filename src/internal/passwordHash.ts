9ae3fddd96e26cb9070792b5eb21affe7e761f6d
Nik Graf <nik@nikgraf.com>
2024-01-08 22:21:15 +0100

    cleanup

--- src/internal/passwordHash.ts
+++ src/internal/passwordHash.ts
@@ -2,11 +2,12 @@ import * as Scrypt from "@noble/hashes/scrypt"
 import * as Data from "effect/Data"
 import * as Effect from "effect/Effect"
 
-class ScryptOptionsError extends Data.Error<{ message: string }> {}
+class OptionsError extends Data.TaggedError("OptionsError")<{
+  cause: unknown
+}> {}
 
 export const scrypt = (input: string | Uint8Array, salt: string | Uint8Array, options: Scrypt.ScryptOpts) =>
   Effect.try({
     try: () => Scrypt.scrypt(input, salt, options),
-    catch: (error) =>
-      error instanceof Error ? new ScryptOptionsError(error) : new ScryptOptionsError({ message: "Unknown error" })
+    catch: (cause) => new OptionsError({ cause })
   })
