c138ddc24ca1e65d7af1253d0db4e83089614f2b
Nik Graf <nik@nikgraf.com>
2024-01-09 10:30:50 +0100

    add scrypt (#2)

---
+++ test/PasswordHash.test.ts
@@ -0,0 +1,42 @@
+import * as Crypto from "effect-crypto"
+import * as Effect from "effect/Effect"
+import { expect, it } from "vitest"
+
+it("should successfully use scrypt", () => {
+  expect(Effect.runSync(Crypto.scrypt("password", "salt", { N: 2 ** 16, r: 8, p: 1, dkLen: 32 }))).toStrictEqual(
+    new Uint8Array([
+      249,
+      20,
+      156,
+      172,
+      159,
+      169,
+      36,
+      9,
+      104,
+      163,
+      144,
+      69,
+      72,
+      30,
+      58,
+      146,
+      29,
+      11,
+      233,
+      197,
+      112,
+      22,
+      217,
+      25,
+      216,
+      41,
+      77,
+      10,
+      51,
+      250,
+      50,
+      18
+    ])
+  )
+})
